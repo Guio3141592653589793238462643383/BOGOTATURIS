850ded2dc789851abe3c2a1d5de8dbc2ad362454
vanessa <vanessatvtapiero@gmail.com>
2025-08-28 20:25:02 -0500

    Actualizando conexiones

--- backend/app/bd_Relacional/db_connection.py
+++ backend/app/bd_Relacional/db_connection.py
@@ -1,18 +1,102 @@
-import mysql.connector
-def create_connection():
-    config = dict(
-        host="localhost",
-        user="root",
-        password="",
-        database="BogotaTuris",
-        port="3306"
-    )
-    try:
-        Connection = mysql.connector.connect(**config)
-        return Connection
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return None
-    
-conectarBase = create_connection()
+from xmlrpc.client import DateTime
+from sqlalchemy import Date, Time, create_engine, String, Integer, Column, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, mapped_column
+
+connection_url = 'mysql://root@127.0.0.1:3306/BogotaTuris'
+
+engine = create_engine(connection_url)
+
+class Base(DeclarativeBase):
+    pass
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+
+    id_usuario = mapped_column(Integer, primary_key=True)
+    primer_nombre = mapped_column(String(50))
+    segundo_nombre = mapped_column(String(50))
+    primer_apellido = mapped_column(String(50))
+    segundo_apellido = mapped_column(String(50))
+    clave = mapped_column(String(50), unique=True)
+    id_rol = mapped_column(Integer, ForeignKey('rol.id_rol'))
+    id_correo = mapped_column(Integer, ForeignKey('correo.id_correo'))
+    id_nac = mapped_column(Integer, ForeignKey('nacionalidad.id_nac'))
+    id_inte = mapped_column(Integer, ForeignKey('intereses.id_inte'))
+
+
+class Rol(Base):
+    __tablename__ = 'rol'
+
+    id_rol = Column(Integer, primary_key=True, autoincrement=True)
+    rol = Column(String(20), nullable=False)
+
+class Correo(Base):
+    __tablename__ = 'correo'
+
+    id_correo = Column(Integer, primary_key=True, autoincrement=True)
+    correo = Column(String(255), nullable=False)
+
+class Nacionalidad(Base):
+    __tablename__ = 'nacionalidad'
+
+    id_nac = Column(Integer, primary_key=True, autoincrement=True)
+    nacionalidad = Column(String(50), nullable=False)
+
+class Intereses(Base):
+    __tablename__ = 'intereses'
+
+    id_inte = Column(Integer, primary_key=True, autoincrement=True)
+    interes = Column(String(80), nullable=False)
+
+class InteresCategoria(Base):
+    __tablename__ = 'inte_categoria'
+
+    id_inte_cate = Column(Integer, primary_key=True, autoincrement=True)
+    categoria = Column(String(80), nullable=False)
+    id_inte = Column(Integer, ForeignKey('intereses.id_inte'), nullable=False)
+
+class ReporteIncidente(Base):
+    __tablename__ = 'reporte_incidente'
+
+    id_report = Column(Integer, primary_key=True, autoincrement=True)
+    descripcion = Column(String(255), nullable=False)
+    fecha_report = Column(DateTime, nullable=False)
+    estado = Column(String(80), nullable=False)
+    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'), nullable=False)
+
+class Comentarios(Base):
+    __tablename__ = 'comentarios'
+
+    id_com = Column(Integer, primary_key=True)
+    tipo_com = Column(String(50), nullable=False)
+    fecha_com = Column(Date, nullable=False)
+    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'), nullable=False)
+
+class Lugar(Base):
+    __tablename__ = 'lugar'
+
+    id_lugar = Column(Integer, primary_key=True)
+    nombre_lugar = Column(String(60), nullable=False)
+    tipo_lugar = Column(String(60), nullable=False)
+    direccion = Column(String(255), nullable=False)
+    hora_aper = Column(Time, nullable=False)
+    hora_cierra = Column(Time, nullable=False)
+    precios = Column(Integer, nullable=False)
+    id_usuario = Column(Integer, ForeignKey('usuario.id_usuario'), nullable=False)
+
+class Alerta(Base):
+    __tablename__ = 'alerta'
+
+    id_alerta = Column(Integer, primary_key=True)
+    tipo_aler = Column(String(255), nullable=False)
+    fecha_alerta = Column(Date, nullable=False)
+    id_lugar = Column(Integer, ForeignKey('lugar.id_lugar'), nullable=False)
+
+class Rutas(Base):
+    __tablename__ = 'rutas'
+
+    id_ruta = Column(Integer, primary_key=True, autoincrement=True)
+    inicio_ruta = Column(String(50), nullable=False)
+    fin_ruta = Column(String(50), nullable=False)
+    id_lugar = Column(Integer, ForeignKey('lugar.id_lugar'), nullable=False)
 
--- backend/app/bd_Relacional/get_data.py
+++ backend/app/bd_Relacional/get_data.py
@@ -1,24 +1,21 @@
-import mysql.connector
-from db_connection import create_connection
-def fetch_users():
-    connection = create_connection()
-    if connection is None:
-        print("No se pudo conectar a la base de datos.")
-        return 
-    try:
-        cursor = connection.cursor()
-        print("Conexión exitosa a la base de datos.")
-        query = "SELECT * FROM usuario"
-        cursor.execute(query)
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-if __name__ == "__main__":
-    fetch_users()
+from db_connection import Usuario, engine 
+from sqlalchemy import select, insert, update, delete
+conexion = engine.connect()
+#consultando la tabla de usuario 
+stmt = select(Usuario)
+data = conexion.execute(stmt)
+print(data)
+for row in data:
+    print(row)
+    #Insertando en usuario 
+stmt1 = insert(Usuario).values(primer_nombre="Juan", segundo_nombre="Carlos", primer_apellido="Perez", segundo_apellido="Lopez", clave="password123", id_rol=2, id_correo=2, id_nac=2, id_inte=2)
+#actualizando usuario
+stmt2 = update(Usuario).where(Usuario.id_usuario == 3).values(primer_nombre="Jose", segundo_nombre="Juan", primer_apellido="Perez", segundo_apellido="Lopez", clave="password123", id_rol=2, id_correo=2, id_nac=2, id_inte=2)
+#eliminando usuario 
+stmt3 = delete(Usuario).where(Usuario.id_usuario == 3)
+conexion.execute(stmt3)
+
+conexion.commit()
+
+conexion.close()
+
